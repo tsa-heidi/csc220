696d7cab17a56456302e4a954f3abf93d2dc0d6b
Karen Santamaria <sanmakaren@gmail.com>
2018-02-13 19:07:09 -0500

    .

--- p2/gene.py
+++ p2/gene.py
@@ -31,71 +31,56 @@ correctly returns the shortest string that contains both input strings as a subs
 '''
 
 
-
-# def intersection(string1, string2, len_string1, len_string2):
-#     length = 0
-#     result_str = ''
-#     matrix = [[0 for col in range(len_string1)] for row in range(len_string2)]
-#     #print_nice(matrix)
-#     #string1 is associated with horizontal diraction
-#     #string2 is associated with vertical direction
-#
-#     #standard way to loop through 2darray
-#     for col in range (len_string1):
-#         for row in range (len_string2):
-#             matrix[row][col]
-#
-#             if (string1[col] == string2[row]):
-#                 matrix[row][col] = 1
-#
-#     print_nice(matrix)
-
 def intersection(string1, string2, len_string1, len_string2):
     length = 0
     result_str = ''
     matrix = [[0 for col in range(len_string1+1)] for row in range(len_string2+1)]
-    #print_nice(matrix)
     #string1 is associated with horizontal diraction
     #string2 is associated with vertical direction
 
 
+
+    ##start building matrix todo find a better way to insert letters
     for col in range (len_string1):
         for row in range (len_string2):
             if (row == 0):
                 matrix[row][col+1] = string1[col]
-
             if (col == 0):
                 matrix[row+1][col] = string2[row]
 
+    for col in range (1,len_string1+1):
+        for row in range (1,len_string2+1):
+            if (matrix[0][col] == matrix[row][0]):
+                matrix[row][col] = 1
+    matrix[0][0] = "-"
+    ##end building matrix
+
+    long_str = ""
+    long_str_len = 0
+#start searching for matches
+
+    for i in range(1,len_string1+1):
+        for j in range
+        matrix[i][i]="-"
+    help_print(matrix)
 
 
-    #standard way to loop through 2darray
-    # for col in range (len_string1):
-    #     for row in range (len_string2):
-    #         matrix[row][col]
-    #
-    #         if (string1[col] == string2[row]):
-    #             matrix[row][col] = 1
 
-    print_nice(matrix)
+def help_see(matrix):
+    '''see how numbers are changed loop by loop through the matrix'''
+    for col in range (1,len_string1+1):
+        for row in range (1,len_string2+1):
+            matrix[row][col] = "-"
+            help_print(matrix)
 
-
-
-
-def print_nice(matrix):
-    '''used for looking at the matrix in easy way'''
+def help_print(matrix):
+    '''print the matrix is view-friendly way'''
     for i in matrix:
         print(i)
 
 def main():
-
-
     str_lst = ["A","C","T","G"]
-    string2 = "AACCTGT"
-    string1 = "CTGTACG"
+    string1 = "AACCTGT"
+    string2 = "CTGTACG"
     string3 = intersection(string1,string2, len(string1), len(string2))
-
-
-
-
 main()
